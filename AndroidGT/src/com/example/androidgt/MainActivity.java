e21fde56333ce12faa665cbff304c70eb3baf7a6
Sumit Vasudeva <vasudeva148@gmail.com>
2013-03-21 11:59:17 +0530

    Test commit

--- AndroidGT/src/com/example/androidgt/MainActivity.java
+++ AndroidGT/src/com/example/androidgt/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	@Override
+	//runs when the activity is created
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
